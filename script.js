464b2a4f4d1f9cba9ad9f438672134925b80fbf3
Vachesav11 <shadowsrespektq@gmail.com>
2023-11-28 13:56:57 +0300

    add 2 practice

--- script.js
+++ script.js
@@ -8,12 +8,29 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt('один из просмотренных фильмов?', ''),
-    b = prompt('на сколько оцениваете его?', ''),
-    c = prompt('один из просмотренных фильмов?',''),
-    d = prompt('на сколько его оцениваете?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('один из просмотренных фильмов?', ''),
+          b = prompt('на сколько оцениваете его?', '');
+
+    if (a != null && b != null && a !== '' && b !== '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотренно довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >=30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Проищошла ошибка");
+}
+
 
 console.log(personalMovieDB)
