518fa4bdb5435e182ad3ae0b429f37e9aa2a9f5c
Vachesav11 <shadowsrespektq@gmail.com>
2023-12-18 20:59:20 +0300

    add practice 4

---
+++ oll/chapterThree/Map/script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const user = {
+   name: 'Alex',
+   surname: 'Smith',
+   birthday: '20/04/1999',
+   showMyPublicData: function () {
+       console.log(`${this.name} ${this.surname}`);
+   }
+}
+const userMap = new Map(Object.entries(user));
+//console.log(userMap);
+
+const newUserObj = Object.fromEntries(userMap);
+console.log(newUserObj);
+
+const shops = [
+    {rise: 500},
+    {oil: 200},
+    {bread: 50}
+];
+
+const budget = [5000, 15000, 25000];
+
+const map = new Map();
+
+shops.forEach((shop, i) => {
+    map.set(shop, budget[i]);
+})
+
+//map.set(shops[0], 5000)
+//    .set(shops[1], 15000)
+//    .set(shops[2], 25000);
+
+console.log(map);
+//console.log(map.get(shops[0]));
+//console.log(map.has(shops[0]));
+//map.delete(key);
+//map.clear();
+//map.size;
+//map.keys();//возвращает по ключам
+
+//список всех товаров во всех магазинах
+
+// const goods = [];
+// for (let shop of map.keys()){
+//     //console.log(shop)
+//     goods.push(Object.keys(shop)[0])
+// }
+// console.log(goods);
+
+// for (let price of map.values()){
+//     console.log(price);
+// }
+
+// for (let [shop, price] of map.entries()){
+//     console.log(price, shop);
+// }
+//
+// map.forEach((value , key , map) => {
+//     console.log(key,value);
+// })
