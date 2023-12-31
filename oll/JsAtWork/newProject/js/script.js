c4c18da2eb4aed374cd0b4df08906194b8173761
Vachesav11 <shadowsrespektq@gmail.com>
2023-12-31 14:20:34 +0300

    add practice 4

--- oll/JsAtWork/newProject/js/script.js
+++ oll/JsAtWork/newProject/js/script.js
@@ -106,12 +106,15 @@ window.addEventListener('DOMContentLoaded', () => {
         modal = document.querySelector('.modal'),
         modalCloseBtn = document.querySelector('[data-close]');
 
-    modalTrigger.addEventListener('click', () => {
-         modal.classList.add('show');
-         modal.classList.remove('hide');
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
         //modal.classList.toggle('show');
         document.body.style.overflow = 'hidden';
-    });
+        clearInterval(modalTimerId);
+    }
+
+    modalTrigger.addEventListener('click', openModal);
 
     function closeModal() {
         modal.classList.add('hide');
@@ -132,4 +135,16 @@ window.addEventListener('DOMContentLoaded', () => {
            closeModal();
        }
     });
+
+    const modalTimerId  = setTimeout(openModal, 5000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });
+
